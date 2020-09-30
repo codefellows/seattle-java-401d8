4efc1cb5c84546827a5dbcef174ba715fb1a8e8b
Nicholas Carignan <nick.carignan@sbcglobal.net>
2020-09-30 09:41:51 -0700

    animal shelter

---
+++ class-13/code-review/animalShelter/src/main/java/animalShelter/Cat.java
@@ -0,0 +1,8 @@
+package animalShelter;
+
+public class Cat extends Animal{
+
+    public Cat(String name){
+        super(name);
+    }
+}
---
+++ class-13/code-review/animalShelter/src/main/java/animalShelter/Dog.java
@@ -0,0 +1,7 @@
+package animalShelter;
+
+public class Dog extends Animal{
+    public Dog(String name){
+        super(name);
+    }
+}
